6c42b0d59e8a836ae63806ee8621beb1a4c70461
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-01 23:21:18 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,8 +24,8 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
+  shipping_manifest["pairs of boots"] = 4
   #your code here
   #remember to return the shipping_manifest hash
-
+  shipping_manifest
 end
